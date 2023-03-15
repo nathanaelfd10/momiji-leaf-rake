51b29f19f201b11b24bf89711b5acc3e28b1ca40
Fernando N <64892153+nathanaelfd10@users.noreply.github.com>
2023-03-15 19:48:28 +0700

    Add Spring Boot project

---
+++ src/main/java/com/noxfl/leafrake/LeafrakeApplication.java
@@ -0,0 +1,13 @@
+package com.noxfl.leafrake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeafrakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeafrakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noxfl/leafrake/LeafrakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noxfl.leafrake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeafrakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
